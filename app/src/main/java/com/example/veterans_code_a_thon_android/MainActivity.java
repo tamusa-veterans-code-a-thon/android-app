216ac155ce883f55ffdd47ffd20000b88d1eb466
Leonard Box <lfbox7@yahoo.com>
2021-05-21 10:17:08 -0500

    initial commit

---
+++ app/src/main/java/com/example/veterans_code_a_thon_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.veterans_code_a_thon_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
