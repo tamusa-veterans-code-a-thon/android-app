0d2d9a83a85a87b3108063badafc31d69bf97d18
janet <jzulu01@jaguar.tamu.edu>
2021-05-22 15:03:43 -0500

    login xml

---
+++ app/src/main/java/com/example/veterans_code_a_thon_android/newUser.java
@@ -0,0 +1,14 @@
+package com.example.veterans_code_a_thon_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+}
