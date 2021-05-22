338625bafd5d51ea257398c43ce9a32c39c31410
reycritt <reycrittenden@gmail.com>
2021-05-22 13:02:35 -0500

    Planning for Firebase integration

--- app/src/main/java/com/example/veterans_code_a_thon_android/MainActivity.java
+++ app/src/main/java/com/example/veterans_code_a_thon_android/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //In Flixter, we get database/json update
+        //in the mainactivity
     }
 }
--- app/src/main/java/com/example/veterans_code_a_thon_android/models/Category.java
+++ app/src/main/java/com/example/veterans_code_a_thon_android/models/Category.java
@@ -1,5 +1,6 @@
 package com.example.veterans_code_a_thon_android.models;
 
+import android.util.Log;
 import android.widget.ImageView;
 
 import org.json.JSONArray;
@@ -7,6 +8,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -14,15 +17,26 @@ import java.util.List;
  * of the app
  */
 public class Category {
+    private static final String TAG = "Category";
     String category;
 
     public Category (JSONObject jsonObject) throws JSONException {
         category = jsonObject.getString("");//FireBase?
     }
 
-    public static List<Category> fromJsonArray(JSONArray categoryJsonArray)
+    public static Category[] fromJsonArray(JSONArray categoryJsonArray)
             throws JSONException {
-        List<Category> categories = new ArrayList<>();
+        HashSet<Category> businessCategories = new HashSet<>();
+
+        //for loop through each bussiness
+            //for loop through each business' categories
+                //categories.add(new Category)
+
+        Category categories[] = new Category[businessCategories.size()];
+        //convert hashset to list (iterate hashset, or find helper method)
+        Arrays.sort(categories);
+
+        Log.d(TAG, categories.toString());
 
         return categories;
     }
