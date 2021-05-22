1a76116ab919388556da7430215f259eebd68022
reycritt <reycrittenden@gmail.com>
2021-05-22 16:02:42 -0500

    Some firebase work and messing with item_category

--- app/src/main/java/com/example/veterans_code_a_thon_android/MainActivity.java
+++ app/src/main/java/com/example/veterans_code_a_thon_android/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.veterans_code_a_thon_android;
 // hello, world
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.veterans_code_a_thon_android.models.Business;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    DatabaseReference dbRef =
+            FirebaseDatabase.getInstance().getReference();
+    DatabaseReference cond = dbRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
 
         //In Flixter, we get database/json update
         //in the mainactivity
+
+
+
+    }
+
+    @Override
+    protected void onStart () {
+        super.onStart();
+
+        cond.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Here: " + text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
