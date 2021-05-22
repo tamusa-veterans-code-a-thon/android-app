6fd1ca117a2b32e2701d42523ddb092bfbf6d5d0
reycritt <reycrittenden@gmail.com>
2021-05-22 16:21:08 -0500

    Worked on CategoryAdapter

--- app/src/main/java/com/example/veterans_code_a_thon_android/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/veterans_code_a_thon_android/adapters/CategoryAdapter.java
@@ -1,4 +1,56 @@
 package com.example.veterans_code_a_thon_android.adapters;
 
-public class CategoryAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.veterans_code_a_thon_android.R;
+import com.example.veterans_code_a_thon_android.models.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    Context context;
+    Category[] categories;
+
+    public CategoryAdapter (Context context, Category[] categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View categoryView = LayoutInflater.from(context).
+                inflate(R.layout.item_category,
+                        parent, false);
+        return new ViewHolder(categoryView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
+        Category category = categories[position];
+        holder.bind(category);
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //Get components of item_cateogry
+        }
+
+        public void bind (final Category category) {
+            //Bind components
+        }
+    }
 }
